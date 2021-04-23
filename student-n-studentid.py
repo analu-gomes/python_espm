df682ce9969c9c5db88897a6cd0be9372142305c
Ana Luisa Gomes <56549291+analu-gomes@users.noreply.github.com>
2021-04-23 11:16:04 -0300

    terminei?
    ahn? terminei?

--- student-n-studentid.py
+++ student-n-studentid.py
@@ -31,7 +31,7 @@ def course(coursename,studyarea):
               'VALUES (?,?)')
     cursor.execute(acourse,(coursename,studyarea))
     course_id = cursor.lastrowid
-    conn.commit
+   #conn.commit
     print('The course id is:', course_id)
 
 def student (name,created,age,cpf):
@@ -41,30 +41,30 @@ def student (name,created,age,cpf):
     creates = datetime.datetime.now
     cursor.execute (astudent,(name,created,age,cpf))
     student_id = cursor.lastrowid
-    conn.commit
+    #conn.commit
     print('The student id is: ', student_id)
         
 def update_course(course_id,coursename,studyarea):
     ucourse = ('UPDATE course'
             'SET coursename = ?, studyarea = ?'
             'WHERE course_id = ?' )
-    cursor.execute (ucourse, (coursename,studyarea))
-    conn.commit
+    cursor.execute (ucourse,(coursename,studyarea,course_id))
+    #conn.commit
 
-def update_student(name,created,age,cpf):
+def update_student(name,created,age,cpf,student_id):
     ustudent = ('UPDATE student'
             'SET name = ?, created = ?, age = ?, cpf = ?'
             'WHERE student_id = ?')
-    cursor.execute(ustudent(name,created,age,cpf))
-    conn.commit
+    cursor.execute(ustudent(name,created,age,cpf,student_id))
+    #conn.commit
 
-def delete_course(course_id,coursename,studyarea):
+def delete_course(course_id):
     cursor.execute('DELETE FROM course WHERE course_id = ?', (course_id,))
-    conn.commit()
+    #conn.commit()
 
-def delete_student(student_id,name,created,age,cpf):
-    cursor.execute('DELETE FROM student WHERE student_id = x', (student_id,))
-    conn.commit()
+def delete_student(student_id):
+    cursor.execute('DELETE FROM student WHERE student_id = ?', (student_id,))
+    #conn.commit()
 
 coursename = input('Insira o nome do curso: ')
 studyarea = input('Insira a área do curso: ')
@@ -77,33 +77,34 @@ age = int(input('Idade: '))
 cpf = int(input('CPF: '))
 student(name,created,age,cpf)
 
-####### TUDO ISSO TA DANDO ERRADO TENHO QUE ARRUMA AMANHA
-### import pandas as pd 
-##cursor.execute('SELECT *'
-           ## 'FROM course'
-            ##'LEFT JOIN student ON course.course_id = student.student_id')
+all_students = cursor.execute('''SELECT *
+            FROM student''')
+all_courses = cursor.execute('''SELECT *
+            FROM course''')
+result = cursor.fetchall()
 
-##result = cursor.fetchall
-##df = pd.DataFrame(result,columns=['course_id, coursename, studyarea, student_id, name, created, age, cpf'])
-###df
+for s in all_students:
+    print(s)
+for c in all_courses:
+    print(c)
 
-##course_id = int(input('Insira o código do curso: '))
-##course = input('Insira o nome do curso: ')
-##studyarea = input('Insira área de estudo: ')
-##update_course(course,studyarea)
+course_id = int(input('Insira o código do curso: '))
+course = input('Insira o nome do curso: ')
+studyarea = input('Insira área de estudo: ')
+update_course(course,studyarea,course_id)
 
-##
-#student_id = int(input('Insira o id do estudante: '))
-##name = input('Nome do estudante: ')
-##created = int(input('Data de matrícula: '))
-##age = int(input('Idade: '))
-##cpf = int(input('CPF: '))
-##update_student(name,created,age,cpf)
 
-## course_id = int(input('Insira o código do curso'))
-###delete_course(course_id)
+student_id = int(input('Insira o id do estudante: '))
+name = input('Nome do estudante: ')
+created = int(input('Data de matrícula: '))
+age = int(input('Idade: '))
+cpf = int(input('CPF: '))
+update_student(name,created,age,cpf,student_id)
 
-## student_id= int(input('Insira o código do estudante'))
-##delete_student(student_id)
+course_id = int(input('Insira o código do curso'))
+delete_course(course_id)
+
+student_id= int(input('Insira o código do estudante'))
+delete_student(student_id)
 
 conn.close()
